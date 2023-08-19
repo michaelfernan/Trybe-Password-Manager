5515609313d16f7785ee9aefb262636c1fce12ca
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-08-19 13:20:40 -0300

    requisito 3 e 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import './App.css';
+import React, { useState } from 'react';
 import Form from './components/Form';
 
 function App() {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  const handleToggleForm = () => {
+    setIsFormVisible(!isFormVisible);
+  };
+
+  const handleFormCancel = () => {
+    setIsFormVisible(false);
+  };
+
   return (
     <div>
-      <h1>
-        {' '}
-        Gerenciador de senhas
-      </h1>
-      {' '}
-      <Form />
+      <h1>Gerenciador de senhas</h1>
+      {isFormVisible ? (
+        <Form onCancel={ handleFormCancel } />
+      ) : (
+        <button onClick={ handleToggleForm }>Cadastrar nova senha</button>
+      )}
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Form() {
+function Form({ onCancel }) {
+  const [nomeServico, setNomeServico] = useState('');
+  const [login, setLogin] = useState('');
+  const [senha, setSenha] = useState('');
+  const [url, setUrl] = useState('');
+
+  const isButtonDisabled = nomeServico === ''
+    || login === ''
+    || senha.length < 8
+    || senha.length > 16
+    || !/[0-9]/.test(senha)
+    || !/[a-zA-Z]/.test(senha)
+    || !/[!@#$%^&*]/.test(senha);
+
   return (
     <div>
       <label htmlFor="nomeServico">Nome do serviço:</label>
-      <input type="text" id="nomeServico" />
+      <input
+        type="text"
+        id="nomeServico"
+        value={ nomeServico }
+        onChange={ (e) => setNomeServico(e.target.value) }
+      />
 
       <label htmlFor="login">Login:</label>
-      <input type="text" id="login" />
+      <input
+        type="text"
+        id="login"
+        value={ login }
+        onChange={ (e) => setLogin(e.target.value) }
+      />
 
       <label htmlFor="senha">Senha:</label>
-      <input type="password" id="senha" />
+      <input
+        type="password"
+        id="senha"
+        value={ senha }
+        onChange={ (e) => setSenha(e.target.value) }
+      />
 
       <label htmlFor="url">URL:</label>
-      <input type="text" id="url" />
+      <input
+        type="text"
+        id="url"
+        value={ url }
+        onChange={ (e) => setUrl(e.target.value) }
+      />
 
-      <button>Cadastrar</button>
-      <button>Cancelar</button>
+      <button disabled={ isButtonDisabled }>Cadastrar</button>
+      <button onClick={ onCancel }>Cancelar</button>
     </div>
   );
 }
